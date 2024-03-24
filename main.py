03ecf20176b2ebda5de9d0a5901d55a14473f356
Nestallum <nassimlattab@gmail.com>
2024-03-24 04:21:07 +0100

    ajout d'une méthode qui permet d'écrire un graph dans un fichier apx avec le format correspondant.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import graph_from_apx as gfa, util
+import IO_graph_apx as gfa, util
 
-file = gfa.get_command_args()
-ug = gfa.read_UG_from_file(file)
-
-print(util.Hbs(ug, 'i'))
+UG = {"i":["a","b","g"],"b":["e"],"a":["d"],"c":[],"e":[],"d":[], "g":["d"]}
+# file = gfa.get_command_args()
+# ug = gfa.read_UG_from_file(file)
+# print(util.Hbs(ug, 'i'))
+gfa.write_apx_from_graph("apx2", "test2", UG)
